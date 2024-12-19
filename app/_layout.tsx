ab0118af1a9b6023f137d4837bc043bb36fde05c
hozayves <muhozayves2014@gmail.com>
2024-12-19 23:37:23 +0200

    Building login flow & setting up

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,31 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { Colors } from '@/constants/Colors'
+import { tokenCache } from '@/utils/cache'
+import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
+import { Slot, Stack } from 'expo-router'
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+if (!publishableKey) {
+  throw new Error(
+    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
+  )
 }
+
+const InitialLayout = () => {
+  return (
+    <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: Colors.background } }}>
+      <Stack.Screen name='index' options={{ headerShown: false }} />
+    </Stack>
+  )
+}
+const RootLayout = () => {
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <ClerkLoaded>
+        <InitialLayout />
+      </ClerkLoaded>
+    </ClerkProvider>
+  )
+}
+
+export default RootLayout
---
+++ app/index.tsx
@@ -0,0 +1,121 @@
+import { View, Text, StyleSheet, Image, StatusBar, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { useAuth, useOAuth } from '@clerk/clerk-expo'
+import * as WebBrowser from 'expo-web-browser'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { Colors } from '@/constants/Colors'
+
+export default function index() {
+  const { startOAuthFlow: authGithub } = useOAuth({ strategy: 'oauth_github' })
+  const { startOAuthFlow: authGoogle } = useOAuth({ strategy: 'oauth_google' })
+  const { top } = useSafeAreaInsets()
+
+  const handleGoogleOAuth = async () => {
+    try {
+      const { createdSessionId, setActive } = await authGoogle()
+      console.log("HandleGoogleOAuth ~ createdSessionId:", createdSessionId)
+
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId })
+      }
+    } catch (error) {
+      console.log("HandleGoogleOAuth ~ error:", error)
+    }
+  }
+  const handleGithubOAuth = async () => {
+    try {
+      const { createdSessionId, setActive } = await authGithub
+        ()
+      console.log("HandleGithubOAuth ~ createdSessionId:", createdSessionId)
+
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId })
+      }
+    } catch (error) {
+      console.log("HandleGithubOAuth ~ error:", error)
+    }
+  }
+
+  const openLink = async () => {
+    WebBrowser.openBrowserAsync('https://galaxies.dev')
+  }
+
+  return (
+    <View style={[styles.container, { paddingTop: top }]}>
+      <StatusBar barStyle="dark-content" />
+      <Image source={require("@/assets/images/todoist-logo.png")} style={styles.loginImage} resizeMode='contain' />
+      <Image source={require("@/assets/images/login.png")} style={styles.bannerImage} resizeMode='contain' />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={() => handleGithubOAuth()}>
+          <MaterialCommunityIcons name='github' size={24} />
+          <Text style={styles.buttonText}>Continue with Github</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleGoogleOAuth()}>
+          <MaterialCommunityIcons name='google' size={24} />
+          <Text style={styles.buttonText}>Continue with Google</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => console.log('Hello world')}>
+          <MaterialCommunityIcons name='email' size={24} />
+          <Text style={styles.buttonText}>Continue with Email</Text>
+        </TouchableOpacity>
+        <Text style={styles.termsText}>
+          By continuing you agree to our
+          <Text style={styles.termsLink} onPress={openLink}>
+            Terms of Service
+          </Text> and <Text style={styles.termsLink} onPress={openLink}>
+            Privacy Policy
+          </Text>
+        </Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 40,
+    marginTop: 20
+  },
+  loginImage: {
+    height: 40,
+    alignItems: 'center',
+    alignSelf: 'center'
+  },
+  bannerImage: {
+    height: 280,
+    alignSelf: 'center'
+  },
+  buttonContainer: {
+    gap: 20,
+    marginHorizontal: 40
+  },
+  button: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 12,
+    borderRadius: 6,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: Colors.lightBorder,
+    gap: 10
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: "500"
+  },
+  termsText: {
+    textAlign: 'center',
+    fontSize: 12,
+    fontWeight: "500",
+    color: Colors.lightText
+  },
+  termsLink: {
+    color: Colors.lightText,
+    fontSize: 12,
+    fontWeight: "500",
+    textDecorationLine: 'underline',
+    textDecorationColor: Colors.lightText,
+    textAlign: 'center'
+  }
+})
