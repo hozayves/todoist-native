b631bfd66a0b78b1101a42926cafd3d203d4e321
hozayves <muhozayves2014@gmail.com>
2025-01-07 20:43:46 +0200

    Implement options for screen presentation to formSheet

--- app/(authenticated)/_layout.tsx
+++ app/(authenticated)/_layout.tsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import { Stack } from 'expo-router'
 import { Colors } from '@/constants/Colors'
+import { useWindowDimensions } from 'react-native'
 
 const Layout = () => {
+    const { height } = useWindowDimensions()
     return (
         <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: Colors.background } }}>
             <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
-            <Stack.Screen name='task/new' options={{ presentation: 'modal' }} />
-            <Stack.Screen name='task/[id]' options={{ presentation: 'modal' }} />
+            <Stack.Screen name='task/new' options={{
+                presentation: 'formSheet',
+                sheetAllowedDetents: height < 700 ? [0.22] : 'fitToContents',
+                sheetGrabberVisible: true, // for ios
+                sheetExpandsWhenScrolledToEdge: false, // for ios
+                sheetCornerRadius: 10, // for ios
+            }} />
+            <Stack.Screen name='task/[id]' options={{
+                presentation: 'formSheet',
+                sheetAllowedDetents: height < 700 ? [0.22] : 'fitToContents',
+                sheetGrabberVisible: true, // for ios
+                sheetExpandsWhenScrolledToEdge: false, // for ios
+                sheetCornerRadius: 10, // for ios
+            }} />
         </Stack>
     )
 }
