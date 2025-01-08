d3622f11009382ec99782cfb9d343fe57c068f67
hozayves <muhozayves2014@gmail.com>
2025-01-08 15:05:34 +0200

    Implement resuable bootsheet component

---
+++ components/BottomSheet.tsx
@@ -0,0 +1,50 @@
+import { Colors } from "@/constants/Colors";
+import { BottomSheetBackdrop, BottomSheetModal, BottomSheetView } from "@gorhom/bottom-sheet";
+import { forwardRef, useCallback, useMemo } from "react";
+import { Text, StyleSheet, View } from "react-native";
+import TodoForm from "./TodoForm";
+
+interface Props {
+    title: string;
+    children: React.ReactNode;
+}
+type Ref = BottomSheetModal;
+
+const BottomSheet = forwardRef<Ref, Props>((props, ref) => {
+    const snapPoints = useMemo(() => [], []);
+
+    const renderBackdrop = (props: any) => (
+        <BottomSheetBackdrop
+            appearsOnIndex={0}
+            disappearsOnIndex={-1} {...props}
+        />)
+
+    return (
+        <BottomSheetModal
+            ref={ref}
+            index={0}
+            snapPoints={snapPoints}
+            handleIndicatorStyle={{ backgroundColor: Colors.dark }}
+            backgroundStyle={{ backgroundColor: Colors.background }}
+            backdropComponent={renderBackdrop}
+            style={styles.contentContainer}
+            enablePanDownToClose={true}
+            enableDynamicSizing={true}
+        >
+            <BottomSheetView>
+                {props.children}
+            </BottomSheetView>
+        </BottomSheetModal>
+    )
+});
+
+const styles = StyleSheet.create({
+    contentContainer: {
+        flex: 1,
+        backgroundColor: Colors.background,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+    }
+});
+
+export default BottomSheet;
