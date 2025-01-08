45827196adbcc97056ced84346cbfa63f270779b
hozayves <muhozayves2014@gmail.com>
2025-01-08 15:36:06 +0200

    Remove screen route for new task and view it, then moved in bottomsheet instead of screen

--- app/(authenticated)/_layout.tsx
+++ app/(authenticated)/_layout.tsx
@@ -8,20 +8,6 @@ const Layout = () => {
     return (
         <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: Colors.background } }}>
             <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
-            <Stack.Screen name='task/new' options={{
-                presentation: 'formSheet',
-                sheetAllowedDetents: height < 700 ? [0.22] : 'fitToContents',
-                sheetGrabberVisible: true, // for ios
-                sheetExpandsWhenScrolledToEdge: false, // for ios
-                sheetCornerRadius: 10, // for ios
-            }} />
-            <Stack.Screen name='task/[id]' options={{
-                presentation: 'formSheet',
-                sheetAllowedDetents: height < 700 ? [0.22] : 'fitToContents',
-                sheetGrabberVisible: true, // for ios
-                sheetExpandsWhenScrolledToEdge: false, // for ios
-                sheetCornerRadius: 10, // for ios
-            }} />
         </Stack>
     )
 }
