244f1f0a609a009712912eaec5835218f2dd8a36
hozayves <muhozayves2014@gmail.com>
2025-01-11 02:35:20 +0200

    Resolve an issue to select date of new task

--- components/TodoForm.tsx
+++ components/TodoForm.tsx
@@ -4,10 +4,15 @@ import { Project, Todo } from "@/types/interfaces"
 import { drizzle, useLiveQuery } from "drizzle-orm/expo-sqlite"
 import { useSQLiteContext } from "expo-sqlite"
 import { projects, todos } from "@/db/schema"
-import { useEffect, useState } from "react"
-import { Colors } from "@/constants/Colors"
+import { useEffect, useRef, useState } from "react"
+import { Colors, DATE_COLORS } from "@/constants/Colors"
 import { Ionicons } from "@expo/vector-icons"
 import { eq } from "drizzle-orm"
+import { BottomSheetModal } from "@gorhom/bottom-sheet"
+import DateSelect from "@/app/(authenticated)/task/Date-select"
+import BottomSheet from "@/components/BottomSheet"
+import * as SecureStore from 'expo-secure-store'
+import { format, isSameDay, isTomorrow } from "date-fns"
 
 type TodoFormProps = {
     todo?: Todo & { project_name: string, project_color: string, project_id: number }
@@ -20,6 +25,7 @@ const TodoForm = ({ todo }: TodoFormProps) => {
     const db = useSQLiteContext()
     const drizzleDb = drizzle(db)
 
+    // FORM HANDLING 
     const {
         control,
         handleSubmit,
@@ -34,6 +40,8 @@ const TodoForm = ({ todo }: TodoFormProps) => {
     });
 
     const { data } = useLiveQuery(drizzleDb.select().from(projects))
+
+    // PROJECT SELECTION 
     const [selectedProject, setSelectedProject] = useState<Project>(
         todo?.project_id
             ? {
@@ -48,6 +56,21 @@ const TodoForm = ({ todo }: TodoFormProps) => {
             }
     )
 
+    const [selectedDate, setSelectedDate] = useState<Date>(
+        todo?.due_date ? new Date(todo.due_date) : new Date()
+    )
+    const [previouslySelectedDate, setPreviouslySelectedDate] = useState<string | undefined>(undefined)
+
+    useEffect(() => {
+        SecureStore.getItemAsync('selectedDate').then((date) => {
+            if (date !== null) {
+                setSelectedDate(new Date(date))
+                setPreviouslySelectedDate(undefined)
+            }
+        })
+    }, [])
+
+    // SUBMIT HANDLING 
     const onSubmit: SubmitHandler<TodoFormData> = async (data) => {
         if (todo) {
             // UPDATE
@@ -56,7 +79,7 @@ const TodoForm = ({ todo }: TodoFormProps) => {
                 name: data.name,
                 description: data.description,
                 project_id: selectedProject.id,
-                due_date: 0, // TODO: add due date
+                due_date: selectedDate.getTime(), // TODO: add due date
             }).where(eq(todos.id, todo.id))
         } else {
             // CREATE
@@ -68,13 +91,41 @@ const TodoForm = ({ todo }: TodoFormProps) => {
                 priority: 0,
                 date_added: Date.now(),
                 completed: 0,
-                due_date: 0,
+                due_date: selectedDate.getTime(),
             })
         }
     }
     useEffect(() => {
         trigger()
     }, [trigger])
+
+    // DATE SELECT BOTTOM SHEET
+    const bottomSheetDateRef = useRef<BottomSheetModal>(null)
+
+    const openDateSelect = () => {
+        SecureStore.setItemAsync('selectedDate', selectedDate.toISOString())
+        setPreviouslySelectedDate(selectedDate.toISOString())
+        bottomSheetDateRef.current?.present()
+    }
+
+    const getDateObject = (date: Date) => {
+        if (isSameDay(date, new Date())) {
+            return {
+                name: 'Today',
+                color: DATE_COLORS.today
+            }
+        } else if (isTomorrow(new Date(date))) {
+            return {
+                name: 'Tomorrow',
+                color: DATE_COLORS.tomorrow
+            }
+        } else {
+            return {
+                name: format(new Date(date), 'EEE, d MMM'),
+                color: DATE_COLORS.other
+            }
+        }
+    }
     return (
         <KeyboardAvoidingView
             enabled
@@ -126,16 +177,19 @@ const TodoForm = ({ todo }: TodoFormProps) => {
                     showsHorizontalScrollIndicator={false}
                     style={styles.actionButtonsContainer}
                 >
-                    <Pressable style={({ pressed }) => {
-                        return [
-                            styles.outlineButton,
-                            {
-                                backgroundColor: pressed ? Colors.lightBorder : 'transparent'
-                            }
-                        ]
-                    }}>
-                        <Ionicons name="calendar-outline" size={24} color={Colors.dark} />
-                        <Text style={styles.outlineButtonText}>Date</Text>
+                    <Pressable
+                        onPress={openDateSelect}
+                        style={({ pressed }) => {
+                            return [
+                                styles.outlineButton,
+                                {
+                                    backgroundColor: pressed ? Colors.lightBorder : 'transparent',
+                                    borderColor: getDateObject(selectedDate).color
+                                }
+                            ]
+                        }}>
+                        <Ionicons name="calendar-outline" size={24} color={getDateObject(selectedDate).color} />
+                        <Text style={[styles.outlineButtonText, { color: getDateObject(selectedDate).color }]}>{getDateObject(selectedDate).name}</Text>
                     </Pressable>
                     <Pressable style={({ pressed }) => {
                         return [
@@ -187,6 +241,20 @@ const TodoForm = ({ todo }: TodoFormProps) => {
                     </TouchableOpacity>
                 </View>
             </ScrollView>
+            <BottomSheet
+                ref={bottomSheetDateRef}
+                snapPoints={['50%']}
+                index={0}
+            >
+                <DateSelect onSave={
+                    (date: Date) => {
+                        const dateString = date.toISOString()
+                        setSelectedDate(date)
+                        SecureStore.setItemAsync('selectedDate', dateString)
+                        bottomSheetDateRef.current?.dismiss()
+                    }
+                } />
+            </BottomSheet>
         </KeyboardAvoidingView>
     )
 }
